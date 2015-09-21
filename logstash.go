7c4b6e196f2043e2a807e1795b8f77ef8d8cf38f
Carlos Villela <cvillela@thoughtworks.com>
2015-09-21 12:14:03 -0300

    Remove container options and args (were not very useful in practice)

--- logstash.go
+++ logstash.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"net"
 	"os"
-	"strings"
 
 	"github.com/gliderlabs/logspout/router"
 )
@@ -47,51 +46,12 @@ func NewLogstashAdapter(route *router.Route) (router.LogAdapter, error) {
 	}, nil
 }
 
-func GetLogspoutOptionsString(env []string) string {
-	if env != nil {
-		for _, value := range env {
-			if strings.HasPrefix(value, "LOGSPOUT_OPTIONS=") {
-				return strings.TrimPrefix(value, "LOGSPOUT_OPTIONS=")
-			}
-		}
-	}
-	return ""
-}
-
-func UnmarshalOptions(opt_string string) map[string]string {
-	var options map[string]string
-
-	if opt_string != "" {
-		b := []byte(opt_string)
-
-		json.Unmarshal(b, &options)
-		return options
-	}
-	return nil
-}
-
 // Stream implements the router.LogAdapter interface.
 func (a *LogstashAdapter) Stream(logstream chan *router.Message) {
-
-	options := UnmarshalOptions(getopt("OPTIONS", ""))
-
 	for m := range logstream {
 		var msg interface{}
-
-		container_options := UnmarshalOptions(GetLogspoutOptionsString(m.Container.Config.Env))
-
-		// We give preference to the containers environment that is sending us the message
-		if container_options == nil {
-			container_options = options
-		} else if options != nil {
-			for k, v := range options {
-				if _, ok := container_options[k]; !ok {
-					container_options[k] = v
-				}
-			}
-		}
-
 		var jsonMsg map[string]interface{}
+
 		err := json.Unmarshal([]byte(m.Data), &jsonMsg)
 		if err != nil {
 			// the message is not in JSON make a new JSON message
@@ -101,8 +61,6 @@ func (a *LogstashAdapter) Stream(logstream chan *router.Message) {
 				ID:       m.Container.ID,
 				Image:    m.Container.Config.Image,
 				Hostname: m.Container.Config.Hostname,
-				Args:     m.Container.Args,
-				Options:  container_options,
 			}
 
 		} else {
@@ -111,8 +69,6 @@ func (a *LogstashAdapter) Stream(logstream chan *router.Message) {
 			jsonMsg["docker.id"] = m.Container.ID
 			jsonMsg["docker.image"] = m.Container.Config.Image
 			jsonMsg["docker.hostname"] = m.Container.Config.Hostname
-			jsonMsg["docker.args"] = m.Container.Args
-			jsonMsg["options"] = container_options
 			msg = jsonMsg
 		}
 
@@ -132,11 +88,9 @@ func (a *LogstashAdapter) Stream(logstream chan *router.Message) {
 
 // LogstashMessage is a simple JSON input to Logstash.
 type LogstashMessage struct {
-	Message  string            `json:"message"`
-	Name     string            `json:"docker.name"`
-	ID       string            `json:"docker.id"`
-	Image    string            `json:"docker.image"`
-	Hostname string            `json:"docker.hostname"`
-	Args     []string          `json:"docker.args,omitempty"`
-	Options  map[string]string `json:"options,omitempty"`
+	Message  string `json:"message"`
+	Name     string `json:"docker.name"`
+	ID       string `json:"docker.id"`
+	Image    string `json:"docker.image"`
+	Hostname string `json:"docker.hostname"`
 }
